67d47ea37e77b6377dda7706e50ea8e858b2b5d3
verysquishyseal <tate.c.larson@icloud.com>
2025-10-03 17:14:03 -0700

    happytime

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,101 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
+//DO CONFIG FUNTIME HERE
+
+//LEFT MOTORS
+constexpr int SP_DRIVE_L1 = -20;
+constexpr int SP_DRIVE_L2 = -15;
+constexpr int SP_DRIVE_L3 = 4;
+//RIGHT MOTORS
+constexpr int SP_DRIVE_R1 = -5;
+constexpr int SP_DRIVE_R2 = -8;
+constexpr int SP_DRIVE_R3 = 2;  
+
+
+
+pros::MotorGroup left_motor_group({SP_DRIVE_L1, SP_DRIVE_L2, SP_DRIVE_L3},
+	pros::MotorGearset::blue
+);
+pros::MotorGroup right_motor_group({SP_DRIVE_R1, SP_DRIVE_R2, SP_DRIVE_R3},
+	pros::MotorGearset::blue
+);
+
+lemlib::Drivetrain drivetrain(
+	&left_motor_group, // left motor group
+    &right_motor_group, // right motor group
+    10, // 10 inch track width
+    lemlib::Omniwheel::NEW_4, // using new 4" omnis
+    360, // drivetrain rpm is 360
+    2 // horizontal drift is 2 (for now)
+);
+
+// imu
+pros::Imu imu(10);
+// horizontal tracking wheel encoder
+pros::Rotation horizontal_encoder(20);
+// vertical tracking wheel encoder
+pros::adi::Encoder vertical_encoder('C', 'D', true);
+// horizontal tracking wheel
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_275, -5.75);
+// vertical tracking wheel
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::NEW_275, -2.5);
+
+// odometry settings
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            &horizontal_tracking_wheel, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(
+	10, // proportional gain (kP)
+    0, // integral gain (kI)
+    3, // derivative gain (kD)
+    3, // anti windup
+    1, // small error range, in inches
+    100, // small error range timeout, in milliseconds
+    3, // large error range, in inches
+    500, // large error range timeout, in milliseconds
+    20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(
+	2, // proportional gain (kP)
+    0, // integral gain (kI)
+    10, // derivative gain (kD)
+    3, // anti windup
+    1, // small error range, in degrees
+    100, // small error range timeout, in milliseconds
+    3, // large error range, in degrees
+    500, // large error range timeout, in milliseconds
+    0 // maximum acceleration (slew)
+);
+
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors // odometry sensors
+);
+
+void initialize() {
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
+}
+
 
 /**
  * A callback function for LLEMU's center button.
@@ -23,12 +119,7 @@ void on_center_button() {
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
 
-	pros::lcd::register_btn1_cb(on_center_button);
-}
 
 /**
  * Runs while the robot is in the disabled state of Field Management System or
@@ -36,6 +127,8 @@ void initialize() {
  * the robot is enabled, this task will exit.
  */
 void disabled() {}
+
+
 
 /**
  * Runs after initialize(), and before autonomous when connected to the Field
@@ -77,8 +170,6 @@ void autonomous() {}
  
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
 
 
 	while (true) {
@@ -89,8 +180,9 @@ void opcontrol() {
 		// Arcade control scheme
 		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
 		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
+		left_motor_group.move(dir - turn);                      // Sets left motor voltage
+		right_motor_group.move(dir + turn);                     // Sets right motor voltage
 		pros::delay(20);                               // Run for 20 ms then update
 	}
 }
+
